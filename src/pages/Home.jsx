4545fecaf9f93ca4add0d40f7658cbc88ac24c6f
brandoll0530 <branollguzman@gmail.com>
2023-12-12 20:24:10 -0600

    commit

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import Footer from '../components/Footer'
 import { setNameTrainer } from '../slices/nameTrainer.slice'
 import { useDispatch } from 'react-redux'
 import { useNavigate } from 'react-router-dom'
@@ -19,24 +18,23 @@ const Home = () => {
   return (
     <section className='min-h-screen grid grid-rows-[1fr_auto]'>
         {/* Parte superior  */}
-        <section  className=" flex justify-center items-center">
+        <section  className="flex justify-center items-center">
             <article>
-                <div>
-                    <img src="/images/pokedex.png" alt="" />
+                <div className="flex justify-center items-center">
+                    <img src="/images/K-POP.jpg" alt="" className="w-[350px] h-[250px]"/>
                 </div>
                 <div className="text-center mt-6"> 
-                <h2 className='font-bold text-5xl text-[#FE1936]'>¡hello trainer!</h2>
-                <p className=' mb-4 font-semibold mt-3'>Give me your name to start:</p>
+                <h2 className='text-5xl font-bold bg-gradient-to-l from-orange-500 to-yellow-400 bg-clip-text text-transparent'>¡Primera convención nacional de Fandoms!</h2>
+                <h2 className='text-3xl font-bold bg-gradient-to-l from-orange-500 to-yellow-400 bg-clip-text text-transparent'>Costa Rica </h2>
+                <p className=' mb-4 font-bold mt-3 bg-gradient-to-l from-orange-500 to-yellow-400 bg-clip-text text-transparent'>Ingresa tu nombre para empezar:</p>
                 <form onSubmit={handleSubmit}>
-                    <input id='nameTrainer' className="bg-[#FFFFFF] shadow-xl  p-2 px-10 border border-red-500" type="text" placeholder='Your name...'/>
-                    <button className="bg-[#D93F3F] text-white py-2 px-4 transition duration-300 transform hover:scale-110 focus:scale-110 mt-4">Start!</button>
+                    <input id='nameTrainer' className="bg-[#FFFFFF] shadow-xl  p-2 px-10 border border-orange-500" type="text" placeholder='Tu nombre aca...'/>
+                    <button className="bg-orange-500 text-white py-2 px-4 transition duration-300 transform hover:scale-110 focus:scale-110 mt-4">comenzar !</button>
                 </form>
                 </div>
             </article>
 
         </section>
-        {/* Footer */}
-        <Footer />
     </section>
   )
 }
--- src/pages/Pokedex.jsx
+++ src/pages/Pokedex.jsx
@@ -40,71 +40,71 @@ const Pokedex = () => {
 
     // Paginas que se van a mostrar en el bloque actual 
     const pagesInBlock = []
-    const minPage = (actualBlock - 1) * PAGES_PER_BLOCK + 1 
+    const minPage = (actualBlock - 1) * PAGES_PER_BLOCK + 1
     const maxPege = actualBlock * PAGES_PER_BLOCK
     for (let i = minPage; i <= maxPege; i++) {
-          if(i <= lastPage){
-            pagesInBlock.push(i)
-          }
-        }
+      if (i <= lastPage) {
+        pagesInBlock.push(i)
+      }
+    }
 
-        return {
-          pokemonInPege,
-          lastPage,
-          pagesInBlock
-        }
+    return {
+      pokemonInPege,
+      lastPage,
+      pagesInBlock
+    }
   }
 
-  const {pokemonInPege,
-          lastPage,
-          pagesInBlock
-        } = paginationLogic()
+  const { pokemonInPege,
+    lastPage,
+    pagesInBlock
+  } = paginationLogic()
 
-        const handleClickPreviusPage = () => {
-          const newCurrentPage = currentPage - 1 
-          if (newCurrentPage >= 1) {
-            setCurrentPage(newCurrentPage)
-          }
-          
-        }
+  const handleClickPreviusPage = () => {
+    const newCurrentPage = currentPage - 1
+    if (newCurrentPage >= 1) {
+      setCurrentPage(newCurrentPage)
+    }
 
-        const handleClickNextPage = () => {
-          const nextCurrentPage = currentPage + 1 
-          if (nextCurrentPage <= lastPage) {
-            setCurrentPage(nextCurrentPage)
-          }
-          
-        }
-  
-    useEffect(() => {
-      if (!currentType) {
+  }
+
+  const handleClickNextPage = () => {
+    const nextCurrentPage = currentPage + 1
+    if (nextCurrentPage <= lastPage) {
+      setCurrentPage(nextCurrentPage)
+    }
+
+  }
+
+  useEffect(() => {
+    if (!currentType) {
       const URL = "https://pokeapi.co/api/v2/pokemon?limit=1281"
 
       axios
         .get(URL)
         .then((res) => setpokemons(res.data.results))
         .catch((err) => console.log(err))
-      }
-    }, [currentType])
+    }
+  }, [currentType])
 
-    useEffect(() => {
-      
-        const URL = "https://pokeapi.co/api/v2/type"
-      
-      axios
-        .get(URL)
-        .then((res) => {
-          const newTypes = res.data.results.map(type => type.name)
-          setTypes(newTypes)
-        })
-        .catch((err) => console.log(err))
-      
-    }, [])
-    
-     useEffect(() => {
-      if (currentType) {
+  useEffect(() => {
+
+    const URL = "https://pokeapi.co/api/v2/type"
+
+    axios
+      .get(URL)
+      .then((res) => {
+        const newTypes = res.data.results.map(type => type.name)
+        setTypes(newTypes)
+      })
+      .catch((err) => console.log(err))
+
+  }, [])
+
+  useEffect(() => {
+    if (currentType) {
       const URL = `https://pokeapi.co/api/v2/type/${currentType}/`
-      
+
       axios
         .get(URL)
         .then((res) => {
@@ -112,19 +112,19 @@ const Pokedex = () => {
           setpokemons(pokemonByType)
         })
         .catch((err) => console.log(err))
-      }
-    }, [currentType])
+    }
+  }, [currentType])
 
-    useEffect(() => {
-      setCurrentPage(1)
+  useEffect(() => {
+    setCurrentPage(1)
   }, [pokemonName, currentType])
 
   useEffect(() => {
     setPokemonName("")
     input.current.value = ""
   }, [currentType])
-  
-    
+
+
   return (
 
     <section className='min-h-screen'>
@@ -132,29 +132,29 @@ const Pokedex = () => {
       <Header />
       {/* SECCION DE FILTROS Y SALUDOS */}
       <section className=' py-5 px-3'>
-        <div className='w-full sm:w-auto'> 
-        <h3><span className=' text-red-500 font-semibold'>Welcome {nameTrainer},</span> here you can find your favorite Pokemon</h3>
-        <form onSubmit={handleSubmit} className='flex items-center justify-center'>
-          <div className=' grid grid-cols-1 sm:grid-cols-none  items-center sm:justify-center sm:w-full mt-10'> 
-          <div className=' max-w-[200px] sm:max-w-[300px] mx-auto flex flex-col items-center sm:flex-row sm:items-center'> 
-          <input ref={input} id='pokemonName' className="bg-[#FFFFFF] shadow-xl  p-2 px-10 mt-4 sm:mt-6 ml-[80px] border-[1px] border-red-500 ms:b" type="text" placeholder='search your pokemon...'/>
-          <button className="bg-[#D93F3F] text-white py-2 px-4 transition duration-300 transform hover:scale-110 focus:scale-110 mt-4  sm:text-center sm:mt-6 sm:mr-[600px]">Search</button>
-          </div>
-          </div>
-          
-          <select onChange={(e) => setCurrentType(e.target.value)} className='bg-[#FFFFFF] shadow-xl py-2 px-12 mt-[230px] sm:mt-6 mr-[120px] sm:mb-[80px] sm:w-[300px] max-w-[200px] sm:max-w-full sm:static duration-300 transform hover:scale-110 focus:scale-110'>
-            <option value="">All pokemon</option>
-            {
-              types.map(type => <option className='capitalize' value={type} key={type}>{type}</option>)
-            }
-          </select>
-        </form>
+        <div className='w-full sm:w-auto'>
+          <h3><span className=' text-red-500 font-semibold'>Welcome {nameTrainer},</span> here you can find your favorite Pokemon</h3>
+          <form onSubmit={handleSubmit} className='flex items-center justify-center'>
+            <div className=' grid grid-cols-1 sm:grid-cols-none  items-center sm:justify-center sm:w-full mt-10'>
+              <div className=' max-w-[200px] sm:max-w-[300px] mx-auto flex flex-col items-center sm:flex-row sm:items-center'>
+                <input ref={input} id='pokemonName' className="bg-[#FFFFFF] shadow-xl  p-2 px-10 mt-4 sm:mt-6 ml-[80px] border-[1px] border-red-500 ms:b" type="text" placeholder='search your pokemon...' />
+                <button className="bg-[#D93F3F] text-white py-2 px-4 transition duration-300 transform hover:scale-110 focus:scale-110 mt-4  sm:text-center sm:mt-6 sm:mr-[600px]">Search</button>
+              </div>
+            </div>
+
+            <select onChange={(e) => setCurrentType(e.target.value)} className='bg-[#FFFFFF] shadow-xl py-2 px-12 mt-[230px] sm:mt-6 mr-[120px] sm:mb-[80px] sm:w-[300px] max-w-[200px] sm:max-w-full sm:static duration-300 transform hover:scale-110 focus:scale-110'>
+              <option value="">All pokemon</option>
+              {
+                types.map(type => <option className='capitalize' value={type} key={type}>{type}</option>)
+              }
+            </select>
+          </form>
         </div>
       </section>
       {/* seccion lista de pokemons  */}
       <section className=' px-2 grid gap-10 auto-rows-auto grid-cols-[repeat(auto-fill,_250px)] justify-center'>
         {
-          pokemonInPege.map(pokemon => <PokemonCart  key={pokemon.url} pokemonUrl={pokemon.url}/>)
+          pokemonInPege.map(pokemon => <PokemonCart key={pokemon.url} pokemonUrl={pokemon.url} />)
         }
       </section>
 
